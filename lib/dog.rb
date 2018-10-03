9317effb9662b40678c4492cd49e7d4716b21241
Sean Lucius <seanlucius22@icloud.com>
2018-10-03 03:17:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,19 @@
 class Dog 
+<<<<<<< HEAD
    def name=(new_dog_name)
     @name = new_dog_name
   end  
   
   def name 
     @name 
+=======
+  def name 
+    @name = name 
+  end 
+  
+  def name=(new_dog_name)
+    @name = new_dog_name
+>>>>>>> 1a5aa19c5381ca4040cf8c5494d6b2eb95ea83c6
   end 
   
   def bark
